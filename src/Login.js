8b652e11397c51f0d86be8333694febc1b047ada
mylycy <mylycybellini@gmail.com>
2021-06-10 11:16:54 -0300

    frist commit

---
+++ src/Login.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+
+const clientId = "962028692872-vq126r85ph9g2tp0m84be913eih3f6e0.apps.googleusercontent.com";
+
+function Login() {
+
+    const [showloginButton, setShowloginButton] = useState(true);
+    const [showlogoutButton, setShowlogoutButton] = useState(false);
+    const onLoginSuccess = (res) => {
+        alert ('Sucesso: as credenciais são válidas!!', res.profileObj)
+        console.log('Login Success:', res.profileObj);
+        setShowloginButton(false);
+        setShowlogoutButton(true);
+    };
+
+    const onLoginFailure = (res) => {
+        alert ('Erro: As credenciais são inválidas!!', res);
+    };
+
+    const onSignoutSuccess = () => {
+        alert("You have been logged out successfully");
+        console.clear();
+        setShowloginButton(true);
+        setShowlogoutButton(false);
+    };
+
+    return (
+        <div>
+            { showloginButton ?
+                <GoogleLogin
+                    clientId={clientId}
+                    buttonText="Sign In"
+                    onSuccess={onLoginSuccess}
+                    onFailure={onLoginFailure}
+                    cookiePolicy={'single_host_origin'}
+                    isSignedIn={true}
+
+                /> : null}
+
+            { showlogoutButton ?
+                <GoogleLogout
+                    clientId={clientId}
+                    buttonText="Sign Out"
+                    onLogoutSuccess={onSignoutSuccess}
+                    export const = "WELCOME"
+                >
+                </GoogleLogout> : null
+            }
+        </div>
+    );
+}
+export default Login;
